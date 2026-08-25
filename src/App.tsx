46bc0bb78bcc507ed780193d7bdacbee44054b07
derek-yen <derekyen@fugle.tw>
2026-08-25 13:31:09 +0800

    chore: add analysis code

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { StoreProvider, useStore } from "./store";
 import BottomNav from "./components/BottomNav";
 import Schedule from "./views/Schedule";
@@ -27,6 +29,8 @@ export default function App() {
   return (
     <StoreProvider>
       <Shell />
+      <Analytics />
+      <SpeedInsights />
     </StoreProvider>
   );
 }
